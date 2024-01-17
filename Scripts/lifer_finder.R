b7d1baa61a0d44d5c695ae767f1d26ac40e8b1fd
smsfrn <61366601+smsfrn@users.noreply.github.com>
2024-01-17 13:59:23 -0600

    reorg file structure; allow for finer resolution output

--- Scripts/lifer_finder.R
+++ Scripts/lifer_finder.R
@@ -16,23 +16,24 @@ library(magick)
 region <- "US" # "US" for whole continental US or "US-NY" for an individual state (replace "NY" with any 2-letter state code).
 user <- "Sam Safran" # enter how you want to be identified in the map caption.
 your_ebird_dat <- here("Data", "sam", "ebird_1705018505439", "MyEBirdData.csv") # path to where your personal eBird data are stored
-needs_list_to_use <- "global" # set to global if you want to map true lifers (species you haven't observed anywhere); set to "regional" if you'd like to map needs for the specified region.
+needs_list_to_use <- "global" # set to "global" if you want to map true lifers (species you haven't observed anywhere); set to "regional" if you'd like to map needs for the specified region.
+resolution <- "9km" # "3km", "9km", or "27km"
 
 # Make directories for user & region
 user_file <- tolower(str_replace(user, " ", ""))
 mainDir <- here("Results")
-dir.create(file.path(mainDir, user_file, region), recursive = TRUE, showWarnings = TRUE)
-outputDir <- here("Results", user_file, region)
-subdirectories <- c("Possible_lifers_weekly_maps", "Possible_lifers_animation")
-lapply(file.path(mainDir, user_file, region, subdirectories), function(x) if (!dir.exists(x)) dir.create(x))
+dir.create(file.path(mainDir, user_file, region, needs_list_to_use, resolution), recursive = TRUE, showWarnings = TRUE)
+outputDir <- here("Results", user_file, region, needs_list_to_use, resolution)
+subdirectories <- c("Weekly_maps", "Animated_map")
+lapply(file.path(mainDir, user_file, region, needs_list_to_use, resolution, subdirectories), function(x) if (!dir.exists(x)) dir.create(x))
 
 # Save region info in df for later use
 region_info <- data.frame(region = region) %>%
   separate(region, into = c("country", "state"), sep = "-", remove = FALSE)
 
 # Get full eBird taxonomy
-sp_all <- rebird::ebirdtaxonomy("species") %>%
-  rename(Common.Name = comName)
+# sp_all <- rebird::ebirdtaxonomy("species") %>%
+#   rename(Common.Name = comName)
 
 # User global life list
 sp_user_all <- read.csv(your_ebird_dat) %>%
@@ -85,11 +86,11 @@ sp_ebst_for_run <- inner_join(sp_ebst, sp_needed) %>%
   filter(species_code != "laugul") # not sure why Laughing Gull is tossing an error
 
 # Download data for needed sp and append paths to a species df
-species_list_paths <- sapply(sp_ebst_for_run$species_code, ebirdst_download_status, download_abundance = FALSE, download_occurrence = TRUE, pattern = "occurrence_median_27km", USE.NAMES = FALSE)
+species_list_paths <- sapply(sp_ebst_for_run$species_code, ebirdst_download_status, download_abundance = FALSE, download_occurrence = TRUE, pattern = paste0("occurrence_median_", resolution), USE.NAMES = FALSE)
 species_df <- data.frame(sp_ebst_for_run, species_list_paths)
 
 # Load occurrence rasters for all species in species_list
-occ_combined <- sapply(sp_ebst_for_run$species_code, load_raster, product = "occurrence", period = "weekly", metric = "median", resolution = "27km")
+occ_combined <- sapply(sp_ebst_for_run$species_code, load_raster, product = "occurrence", period = "weekly", metric = "median", resolution = resolution)
 
 # Vector data for region
 study_area <- ne_states(iso_a2 = "US", returnclass = "sf")
@@ -105,8 +106,18 @@ study_area <- st_transform(study_area, st_crs(occ_combined[[1]]))
 occ_crop_combined <- sapply(occ_combined, crop, y = study_area, USE.NAMES = FALSE, overwrite = TRUE)
 occ_crop_combined <- sapply(occ_crop_combined, trim)
 
+view_sp <- function(x){sp_max <- (raster::raster(max(occ_crop_combined[[x]])))
+                     mapview::mapview(sp_max)}
+# view_sp("chclon")
+
+# some of the included species never have occurrence porbabilities >0 in the region. save resources by filtering them out and not processing their layers.
+sp_maxvals <- lapply(occ_crop_combined, minmax) %>% sapply(max)
+sp_ebst_for_run <- bind_cols(sp_ebst_for_run, as.data.frame(sp_maxvals))
+sp_ebst_for_run_in_region <- filter(sp_ebst_for_run, sp_maxvals > 0) 
+occ_crop_combined_ <- occ_crop_combined[names(occ_crop_combined) %in% sp_ebst_for_run_in_region$species_code]
+
 # Define occurrence threhsold for when a species is "possible"
-possible_occurrence_threshold <- 0.05 # minimum occurrence probabilty for a species to be considered "possible" at a given time/location.
+possible_occurrence_threshold <- 0.01 # minimum occurrence probability for a species to be considered "possible" at a given time/location.
 
 # Sum number of "possible" species in each cell based on occurrence probability and the defined threshold. Each week stored as a single-layer SpatRaster in a list.
 possible_lifers <- list()
@@ -124,15 +135,15 @@ possible_lifers <- sapply(possible_lifers, mask, mask = project(vect(study_area)
 possible_lifers <- sapply(possible_lifers, trim)
 
 # Get maximum lifer count (across all cells and weeks). Needed for fill scale.
-r_c_possible <- rast(possible_lifers)
-max_val_possible <- max(minmax(r_c_possible))
+max_val_possible <- lapply(possible_lifers, minmax) %>% sapply(max) %>% max()
 
 # Generate weekly maps
 bg_color <- "azure"
 week_plots_possible <- list()
 for (i in 1:length(possible_lifers)) {
   date <- occ_crop_combined[[1]]@cpp[["names"]][i]
-  legend_lab <- "Your potential lifers"
+  if(needs_list_to_use == "global"){legend_lab <- "Your potential lifers"}
+  if(needs_list_to_use == "regional"){legend_lab <- "Your regional needs"} 
   week_plot <- ggplot() +
     geom_spatraster(data = possible_lifers[[i]]) +
     geom_sf(data = study_area, fill = NA, color = alpha("white", .3)) +
@@ -185,15 +196,15 @@ Data from 2022 eBird Status & Trends products (https://ebird.org/science/status-
       legend.text.align = 0.5
     )
   week_plot
-  ggsave(filename = here("Results", user_file, region, "Possible_lifers_weekly_maps", paste0(region, "_", date, ".jpg")), plot = week_plot, bg = "white", height = 5.35, width = 6.6)
+  ggsave(filename = here(outputDir, "Weekly_maps", paste0(region, "_", date, ".jpg")), plot = week_plot, bg = "white", height = 5.35, width = 6.6)
   week_plots_possible[[i]] <- week_plot
 }
 
 # Generate animated gif
-imgs <- list.files(here("Results", user_file, region, "Possible_lifers_weekly_maps"), full.names = T)
+imgs <- list.files(here(outputDir, "Weekly_maps"), full.names = T)
 img_joined <- image_join(lapply(imgs, image_read))
 img_animated <- image_animate(img_joined, fps = 5)
-image_write(image = img_animated, path = here("Results", user_file, region, "Possible_lifers_animation", paste0(region, "_Possible_lifers_annual_hires.gif")))
-hires <- image_read(here("Results", user_file, region, "Possible_lifers_animation", paste0(region, "_Possible_lifers_annual_hires.gif")))
+image_write(image = img_animated, path = here(outputDir, "Animated_map", paste0(region, "_Animated_map_annual_hires.gif")))
+hires <- image_read(here(outputDir, "Animated_map", paste0(region, "_Animated_map_annual_hires.gif")))
 lores <- image_scale(hires, geometry_size_percent(width = 38, height = NULL))
-image_write(image = lores, path = here("Results", user_file, region, "Possible_lifers_animation", paste0(region, "_Possible_lifers_annual_lores.gif")))
+image_write(image = lores, path = here(outputDir, "Animated_map", paste0(region, "_Animated_map_annual_lores.gif")))
